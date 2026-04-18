6379ec5d4e13f721103932a577c0b1a729a0b280
umer <m.umer394aziz@gmail.com>
2026-04-19 01:49:05 +0500

    complete app created and error debuged

--- App.tsx
+++ App.tsx
@@ -1,27 +1,54 @@
-import { StyleSheet, Text, View , Image , } from 'react-native'
-import type { PropsWithChildren } from 'react';
-import React from 'react'
-export const DiceImages = {
-  one: require('./Diceone.jpg'),
-  two: require('./Dicetwo.jpg'),
-  three: require('./Dicethree.jpg'),
-  four: require('./Dicefour.jpg'),
-  five: require('./Dicefive.jpg'),
-  six: require('./Dicesix.jpg'),
-};
-type diceprops = PropsWithChildren<
-{
+import React, {useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import { StyleSheet, Text, View, Image, ImageSourcePropType, Pressable } from 'react-native';
+import DiceOne from './dice/Diceone.jpg';
+import DiceTwo from './dice/Dicetwo.jpg';
+import DiceThree from './dice/Dicethree.jpg';
+import DiceFour from './dice/Dicefour.jpg';
+import DiceFive from './dice/Dicefive.jpg';
+import DiceSix from './dice/Dicesix.jpg';
+
+type DiceProps = PropsWithChildren<{
   imageUrl: ImageSourcePropType
 }>
 
-function App() : JSX.Element  {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
 
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View style={styles.diceContainer}>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
+
+export default function App(): JSX.Element {
+
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollDiceOnTap = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1: setDiceImage(DiceOne); break;
+      case 2: setDiceImage(DiceTwo); break;
+      case 3: setDiceImage(DiceThree); break;
+      case 4: setDiceImage(DiceFour); break;
+      case 5: setDiceImage(DiceFive); break;
+      case 6: setDiceImage(DiceSix); break;
+      default: setDiceImage(DiceOne); break;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      
+      <Pressable onPress={rollDiceOnTap}>
+        <Text style={styles.rollDiceBtnText}>Roll the dice</Text>
+      </Pressable>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -49,5 +76,3 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase',
   },
 });
-
-export default App;
